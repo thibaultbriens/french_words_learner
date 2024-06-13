3ac9d71adc3d066eefb03f2fac414a84699b3f19
thibaultbriens <thibault.briens@gmail.com>
2024-06-13 19:54:53 +0200

    added: custom notification hour

--- lib/backend/notifications_service.dart
+++ lib/backend/notifications_service.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:localstore/localstore.dart';
 // for scheduled notif
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -104,11 +105,65 @@ class NotificationSercice {
         return;
       }
     }
+
+    // Get user reminder hour
+    List<int> reminderTime = await getHourMinute();
+
     print("added notif for ${date}");
-    showDelayed(id: dateId(date), title: titles[Random().nextInt(titles.length)], body: "Venez swiper vos mots du jour pour continuer votre apprentissage", date: DateTime(date.year, date.month, date.day, 10));
+    showDelayed(id: dateId(date), title: titles[Random().nextInt(titles.length)], body: "Venez swiper vos mots du jour pour continuer votre apprentissage", date: DateTime(date.year, date.month, date.day, reminderTime[0], reminderTime[1]));
   }
   
   int dateId(DateTime date) {
     return int.parse("${date.day.toString().padLeft(2, "0")}${date.month.toString().padLeft(2, "0")}${date.year}");
+  }
+
+  Future<void> updateTime(int hour, int minute) async{
+    // Save in LocalStore
+    final db = Localstore.instance;
+
+    db.collection("static").doc("notifications").set({
+        "reminder-hour": hour,
+        "reminder-minute": minute
+      }, 
+      SetOptions(merge: true));
+
+    // Update Schedules notifications
+    List<PendingNotificationRequest> pendingNotifs = await _localNotifService.pendingNotificationRequests();
+
+    List<int> ids = []; // This is cool because the id is the dateId : 01012000 for January 1st of 2000
+
+    // Store ids
+    for(int i = 0; i < pendingNotifs.length; i++){
+      ids.add(pendingNotifs[i].id);
+    }
+
+    // Delete All Notficiations
+    await _localNotifService.cancelAll();
+
+    // Schedule notifications
+    for(int id in ids){
+      String idStr = id.toString();
+      DateTime date = DateTime(int.parse("${idStr[4]}${idStr[5]}${idStr[6]}${idStr[7]}"), int.parse("${idStr[2]}${idStr[3]}"), int.parse("${idStr[0]}${idStr[1]}"), hour, minute);
+      showDelayed(id: id, title: titles[Random().nextInt(titles.length)], body: "Venez swiper vos mots du jour pour continuer votre apprentissage", date: date);
+    }
+  }
+
+  // Get custom reminder hour
+  Future<List<int>> getHourMinute() async {
+    List<int> result = [10, 0];
+
+    Map<String, dynamic>? data = await Localstore.instance.collection("static").doc("notifications").get();
+
+    if(data == null) return result;
+
+    if(data.keys.contains("reminder-hour")){
+      result[0] = data["reminder-hour"];
+    }
+
+    if(data.keys.contains("reminder-minute")){
+      result[1] = data["reminder-minute"];
+    }
+
+    return result;
   }
 }
--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:french_words_learner/backend/notifications_service.dart';
 import 'package:french_words_learner/screens/settings/learning_state.dart';
+import 'package:french_words_learner/shared/loader.dart';
+import 'package:localstore/localstore.dart';
+import 'package:provider/provider.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -36,15 +41,133 @@ class Settings extends StatelessWidget {
               ],
             ),
           ),
-          /*ListTile(
-            leading: Icon(Icons.pending),
-            title: Text("Test notifications", style: TextStyle(fontSize: 14),),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => TestNotif()));
-            },
-          )*/
+          SizedBox(height: 10,),
+          Text("Préférences", style: TextStyle(fontSize: 19)),
+          SizedBox(height: 4,),
+          Card(
+            color: Colors.white,
+            child: ListTile(
+              leading: Icon(Icons.access_time_rounded),
+              title: Text("Heure de rappel", style: TextStyle(fontSize: 14),),
+              onTap: () {
+                showDialog(context: context, builder: (context){
+                  return HourSelectorDialog();
+                });
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+class HourSelectorDialog extends StatelessWidget {
+  const HourSelectorDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<int>>(
+      future: NotificationSercice().getHourMinute(),
+      builder: (context, snapshot) {
+
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Loader();
+        }
+
+        if(snapshot.hasError){
+          return Text("Snapshot error");
+        }
+
+        int hour = snapshot.data![0];
+        int minute = snapshot.data![1];
+
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Dialog(
+              child: Container(
+                padding: EdgeInsets.all(25),
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Choisissez une heure", style: TextStyle(fontSize: 19),),
+                    SizedBox(height: 10,),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          width: 55,
+                          child: DropdownButtonFormField<int>(
+                            menuMaxHeight: MediaQuery.of(context).size.height / 3,
+                            value: hour,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(),
+                            ),
+                            icon: Icon(Icons.arrow_drop_down),
+                            items: List.generate( 24, (index) => DropdownMenuItem<int>(value: index, child: Text('$index',),),
+                            ),
+                            onChanged: (int? newValue) {
+                              setState(() {
+                                hour = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+            
+                        SizedBox(width: 5,),
+                        Text(":", style: TextStyle(fontSize: 22),),
+                        SizedBox(width: 5,),
+            
+                        Container(
+                          width: 55,
+                          child: DropdownButtonFormField<int>(
+                            menuMaxHeight: MediaQuery.of(context).size.height / 3,
+                            value: minute,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(),
+                            ),
+                            icon: Icon(Icons.arrow_drop_down),
+                            items: List.generate( 12 /* 60 / 5 */, (index) => DropdownMenuItem<int>(value: index, child: Text('${index * 5}',),),
+                            ),
+                            onChanged: (int? newValue) {
+                              setState(() {
+                                minute = newValue!;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+            
+                    SizedBox(height: 25,),
+            
+                    Container(
+                      alignment: Alignment.centerRight,
+                      height: 40,
+                      child: ElevatedButton(
+                        child: Text("Confirmer", style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)))
+                        ),
+                        onPressed: () async {
+                          // Update notification hour
+                          await NotificationSercice().updateTime(hour, minute);
+                          Navigator.pop(context);
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+        );
+      }
+    );
+  }
+}
+
